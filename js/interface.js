56da941a3dc2911958a1a9f8c8a586a757fcb738
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-05 17:12:02 +0000

    created an interface file using jQuery for the interactive components

--- js/interface.js
+++ js/interface.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
+
+    let getTemperature = (cityName) => {
+        $.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=72e11d5fe6e5b767dd8571d40abf06d6&units=metric`, (response) => {
+        $("#render-temperature").text(
+            cityName + ' is ' + Math.floor(response.main.temp)
+        );
+    })
+    };
+
     let thermostat = new Thermostat();
     displayTemperature();
+    getTemperature('London');
+
+    $('#city-choice').change(function() {
+        getTemperature($(this).val());
+    })
 
     $('#increase-temp').click(function() {
         thermostat.increaseTemp();
